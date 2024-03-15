fb1e921c121ab350f7948576baae2fa5227b9b65
S-Garner <sgarner@cub.uca.edu>
2024-03-15 15:33:36 -0500

    Added a object example

--- src/VicPic.java
+++ src/VicPic.java
@@ -1,7 +1,29 @@
 package src;
 
+import javax.swing.*;
+import src.*;
+import src.IOClasses.CompInstrHolder;
+import src.IOClasses.SignalAndStart;
+import src.Interfaces.Instructions;
+import src.Interfaces.Trigger;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class VicPic {
     public static void main(String[] args){
-        Driver drive = new Driver();
+        //Driver drive = new Driver();
+
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(1000, 600));
+
+        testPanel panel = new testPanel();
+
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
